7fb8cc57f7a3319b673a6d54cf8ed6297aff8d4f
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-30 19:55:50 +0530

    add types

---
+++ src/components/Counter.types.ts
@@ -0,0 +1,11 @@
+import { MouseEventHandler, ReactNode } from "react";
+
+export type CounterProps = object;
+export type counterType = number;
+export type timerStartedType = boolean;
+export type timerType = NodeJS.Timeout;
+
+export type ButtonProps = {
+  children: ReactNode;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+};
