b8de7eb99768136e8d0e70730a04c5d423be78b3
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-30 19:55:26 +0530

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import { ButtonProps } from "./Counter.types";
+
+export const Button: React.FC<ButtonProps> = ({ children, handleClick }) => {
+  return <button onClick={handleClick}>{children}</button>;
+};
