c718809df7ed4af477139e2b53cdff6f1936170f
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-31 11:43:48 +0530

    fix: prettier format

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,14 @@
+/* eslint-disable no-undef */
 import { ButtonProps } from './Counter.types'
 
-export const Button: React.FC<ButtonProps> = ({ children, handleClick }) => {
-  return <button onClick={handleClick}>{children}</button>
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  handleClick,
+  id,
+}) => {
+  return (
+    <button data-testid={id} onClick={handleClick}>
+      {children}
+    </button>
+  )
 }
