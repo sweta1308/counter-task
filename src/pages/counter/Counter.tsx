9876eec4f64049e34c895fb1e3df4f9b863d35f1
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-31 20:45:04 +0530

    counter page

---
+++ src/pages/counter/Counter.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react'
+import './Counter.css'
+import { Navbar } from '../../components/navbar/Navbar'
+
+const Counter = () => {
+  const [counter, setCounter] = useState<number>(0)
+  const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false)
+  // eslint-disable-next-line no-undef
+  const timer = useRef<NodeJS.Timeout | undefined>(undefined)
+
+  const handleClick = () => {
+    setIsTimerStarted((prev) => !prev)
+    if (!isTimerStarted) {
+      timer.current = setInterval(() => {
+        setCounter((prev) => prev + 1)
+      }, 1000)
+    } else {
+      clearInterval(timer.current)
+      timer.current = undefined
+    }
+  }
+
+  const buttonText = isTimerStarted ? 'Stop' : 'Start'
+
+  return (
+    <>
+      <Navbar />
+      <div className="counter">
+        <h1>{counter} seconds</h1>
+        <button className="counter-btn" onClick={handleClick}>
+          {buttonText}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Counter
