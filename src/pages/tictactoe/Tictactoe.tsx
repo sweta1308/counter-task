fde0b7f53959f6aa0cfb88520c2f00afc75bb896
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-31 20:45:37 +0530

    add toc tac toe page

---
+++ src/pages/tictactoe/Tictactoe.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from '../../components/navbar/Navbar'
+
+const Tictactoe = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Tictactoe</h1>
+    </>
+  )
+}
+
+export default Tictactoe
