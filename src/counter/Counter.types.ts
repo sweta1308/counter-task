64cf0b6871c3e6f9e1b5e41daa730c2c98ee47e6
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-31 14:34:19 +0530

    add types for counter

---
+++ src/counter/Counter.types.ts
@@ -0,0 +1,5 @@
+export type CounterProps = object
+export type counterType = number
+export type timerStartedType = boolean
+// eslint-disable-next-line no-undef
+export type timerType = NodeJS.Timeout | null
