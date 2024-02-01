d3244be73bb96965fa96bb3083d365d14a82a0bb
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-01 13:43:46 +0530

    add tic tac toe provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { TictactoeProvider } from 'context/TictactoeContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TictactoeProvider>
+        <App />
+      </TictactoeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
