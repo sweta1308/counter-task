0da07d21824be95113eb44566244e1216b7d4094
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 12:21:20 +0530

    add tests for navbar

---
+++ src/components/navbar/Navbar.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Navbar } from './Navbar'
+import { BrowserRouter } from 'react-router-dom'
+
+describe('Navbar component', () => {
+  test('renders Navbar component', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Counter')).toBeInTheDocument()
+    expect(screen.getByText('Tic Tac Toe')).toBeInTheDocument()
+  })
+})
