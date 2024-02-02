ddfaf50c65f5c86f7946daca53dac1db970158d7
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 17:44:06 +0530

    add provider to tictactoe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Counter, Home, Tictactoe } from './pages/index'
+import { TicTacToeOutlet } from 'components/TicTacToeOutlet'
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/tictactoe" element={<Tictactoe />} />
+        <Route element={<TicTacToeOutlet />}>
+          <Route path="/tictactoe" element={<Tictactoe />} />
+        </Route>
       </Routes>
     </div>
   )
