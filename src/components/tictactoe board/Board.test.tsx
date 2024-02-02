83ba2efc4d245b42fbfede504f6023585a31b836
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 12:21:48 +0530

    add tests for board component

---
+++ src/components/tictactoe board/Board.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Board from './Board'
+import { useTictactoe } from 'context/TictactoeContext'
+import { BrowserRouter } from 'react-router-dom'
+
+jest.mock('context/TictactoeContext', () => ({
+  ...jest.requireActual('context/TictactoeContext'),
+  useTictactoe: jest.fn(),
+}))
+
+describe('Board component', () => {
+  test('renders Board component with initial values', () => {
+    // eslint-disable-next-line prettier/prettier
+    (useTictactoe as jest.Mock).mockReturnValue({
+      boardValues: ['', '', '', '', '', '', '', '', ''],
+      message: "X's turn",
+      handleBoardClick: jest.fn(),
+      handleResetClick: jest.fn(),
+    })
+    render(
+      <BrowserRouter>
+        <Board />
+      </BrowserRouter>,
+    )
+
+    const element = screen.getByTestId('0')
+
+    expect(element).toBeInTheDocument()
+  })
+})
