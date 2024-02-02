200c25beb22c00f737f7053c847c387299e0bd23
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 17:44:18 +0530

    remove tictactoe provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { TictactoeProvider } from 'context/TictactoeContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <TictactoeProvider>
-        <App />
-      </TictactoeProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
