39b4400f04fc2de28db8f4698f6bacd472e61691
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 12:19:55 +0530

    add properties to remove bugs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+
+  // Working directory
+  roots: ['<rootDir>/src'],
+
+  // Transforms
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts|tsx?$': 'ts-jest',
+    '^.+\\.js|jsx?$': '<rootDir>/node_modules/babel-jest',
   },
-  globals: {
-    // TypeScript
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+
+  moduleNameMapper: {
+    '\\.(jpg|JPG|gif|GIF|png|PNG|less|LESS|css|CSS)$':
+      '<rootDir>/src/test/unit/mocks/file.mock.js',
   },
+
+  setupFilesAfterEnv: ['./src/setupTests.js'],
+
+  moduleDirectories: ['node_modules', 'src'],
 }
