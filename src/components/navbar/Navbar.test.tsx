3764e25588e0b9ec0fe733ca3fdcd5dd6dcfdbd5
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-02 17:45:11 +0530

    add tests

--- src/components/navbar/Navbar.test.tsx
+++ src/components/navbar/Navbar.test.tsx
@@ -3,13 +3,56 @@ import { render, screen } from '@testing-library/react'
 import { Navbar } from './Navbar'
 import { BrowserRouter } from 'react-router-dom'
 
-test('renders Navbar component', () => {
-  render(
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>,
-  )
-  expect(screen.getByText('Home')).toBeInTheDocument()
-  expect(screen.getByText('Counter')).toBeInTheDocument()
-  expect(screen.getByText('Tic Tac Toe')).toBeInTheDocument()
+describe('Navbar component', () => {
+  test('renders Navbar component', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Counter')).toBeInTheDocument()
+    expect(screen.getByText('Tic Tac Toe')).toBeInTheDocument()
+  })
+
+  test('renders Navbar component correctly', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+
+    const home = screen.getByText('Home')
+    expect(home).toBeInTheDocument()
+    expect(home).toHaveAttribute('href', '/')
+
+    const counter = screen.getByText('Counter')
+    expect(counter).toBeInTheDocument()
+    expect(counter).toHaveAttribute('href', '/counter')
+
+    const tictactoe = screen.getByText('Tic Tac Toe')
+    expect(tictactoe).toBeInTheDocument()
+    expect(tictactoe).toHaveAttribute('href', '/tictactoe')
+  })
+
+  test('activates the correct link based on the current location', () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        pathname: '/tictactoe',
+      },
+    })
+
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+
+    const tictactoe = screen.getByText('Tic Tac Toe')
+    expect(tictactoe).toHaveClass('active-link')
+    const home = screen.getByText('Home')
+    expect(home).not.toHaveClass('active-link')
+    const counter = screen.getByText('Counter')
+    expect(counter).not.toHaveClass('active-link')
+  })
 })
